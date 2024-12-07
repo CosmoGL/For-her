71751c4bb4ce0e0c9364b98c0f82b5c5b594ee4d
Axel <Axel@DESKTOP-GQ125V7>
2024-12-07 16:00:14 +0800

    Initial Commit.

---
+++ You/src/Her/Hehe.java
@@ -0,0 +1,55 @@
+package Her;
+
+public class Hehe {
+    public static void main(String[] args) {
+        String[] y = {
+            "*     *",
+            " *   * ",
+            "  * *  ",
+            "   *   ",
+            "   *   "
+        };
+
+        String[] u = {
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " * * * "
+        };
+
+        String[] r = {
+            "*******",
+            "*    * ",
+            "*******",
+            "*   *  ",
+            "*    * "
+        };
+
+        String[] a = {
+            "  ***  ",
+            " *   * ",
+            "*******",
+            "*     *",
+            "*     *"
+        };
+
+        String[] i = {
+            "  ***  ",
+            "   *   ",
+            "   *   ",
+            "   *   ",
+            "  ***  "
+        };
+
+        for (int line = 0; line < 5; line++) {
+            System.out.print(y[line] + "  "); 
+            System.out.print(u[line] + "  "); 
+            System.out.print(r[line] + "  "); 
+            System.out.print(a[line] + "  "); 
+            System.out.print(i[line] + "  "); 
+            System.out.print(a[line]);         
+            System.out.println();               
+        }
+    }
+}
